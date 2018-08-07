7ce7f757fb09185c6ac42c0dbaae94378d1504d5
JamesJoe-C <james@james-joe.com>
2018-08-07 10:31:14 +0800

    add 020

--- 020. Valid Parentheses/main.go
+++ 020. Valid Parentheses/main.go
@@ -5,28 +5,29 @@ import (
 )
 
 func main() {
-	fmt.Println(isValid("{}"))
+	//fmt.Println(isValid("()[]{}"))
+	fmt.Println(isValid("["))
+	fmt.Println(isValid("]"))
 }
 
 //未完成
 func isValid(s string) bool {
 	if s == "" {
-		return false
+		return true
 	}
-	temp := make([]string, 1, 2)
+	temp := make(map[int]string)
 	len_s := len(s)
 	j := 0
-	fmt.Println("temp start:", len(temp), temp)
 	for i := 0; i < len_s; i++ {
 		temp_s := string(s[i])
-		fmt.Println("temp_s", temp_s)
+		//fmt.Println("temp_s", temp_s)
 		if temp_s == "(" || temp_s == "{" || temp_s == "[" {
 			j++
-			temp = append(temp, temp_s)
-			fmt.Println("temp:", temp)
+			temp[j] = temp_s
+			//fmt.Println("temp:", temp, temp_s)
 			continue
 		}
-		fmt.Println(j, string(temp[j]))
+		//fmt.Println("i,j,temp[j]=", i, j, string(temp[j]))
 		switch string(temp[j]) {
 		case "(":
 			if s[i] != ")"[0] {
@@ -34,20 +35,28 @@ func isValid(s string) bool {
 			}
 			temp[j] = ""
 			j--
+
 		case "{":
 			if s[i] != "}"[0] {
 				return false
 			}
 			temp[j] = ""
 			j--
+
 		case "[":
 			if s[i] != "]"[0] {
 				return false
 			}
 			temp[j] = ""
 			j--
+		case "":
+			return false
 		}
 
+	}
+	//fmt.Println("end temp:", temp)
+	if j > 1 || temp[1] != "" {
+		return false
 	}
 	return true
 }
